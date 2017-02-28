171916bb6bf4aa57b04c736f011c2820ce74b571
Leo <leowolf749@gmail.com>
2017-02-28 18:49:00 -0500

    Styled the header section and added an image.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,35 @@
 let gulp = require('gulp');
-let sass = require('gulp-sass');
+let sass = require ('gulp-sass');
 let browser = require('gulp-browser');
 
-gulp.task('default', ['html', 'css', 'js']);
+gulp.task('default', ['html', 'css', 'js', 'assets']);
 
-gulp.task('html', function () {
+gulp.task('html', function() {
     return gulp.src('index.html')
-        .pipe(gulp.dest('public/'));
+        .pipe(gulp.dest('docs/'));
 });
 
-gulp.task('css', function () {
-    return gulp.src('scss/style.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('public/'));
+gulp.task('css', function() {
+    return gulp.src('styles/style.scss')
+        .pipe(sass()) 
+        .pipe(gulp.dest('docs/'));
 });
 
-gulp.task('js', function () {
-    return gulp.src('js/app.js')
-        .pipe(browser.browserify())
-        .pipe(gulp.dest('public/'));
+gulp.task('js', function() {
+    return gulp.src('app.js')
+        .pipe(browser.browserify()) 
+        .pipe(gulp.dest('docs/'));
 });
 
-gulp.task('watch', ['default'], function () {
-    gulp.watch('js/*.js', ['js']);
-    gulp.watch('scss/*.scss', ['css']);
+gulp.task('assets', function() {
+    return gulp.src(['assets/*.ico', 'assets/*.jpg', 'assets/*.png', 'assets/*.pdf'])
+        .pipe(gulp.dest('docs/assets'));
+});
+
+gulp.task('watch', ['default'], function() {
     gulp.watch('*.html', ['html']);
+    gulp.watch('styles/*.scss', ['css']);
+    gulp.watch('styles/partials/*.scss', ['css']);
+    gulp.watch('*.js', ['js']);
+    gulp.watch('assets/**.*', ['assets']);
 });
