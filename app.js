604fc41d70b0cc03560a62897d9c2ffc0561f0a3
Leo <leowolf749@gmail.com>
2017-03-31 15:40:46 -0400

    Pushed portfolio projects to surge.

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ window.addEventListener('load', function () {
             tools: 'Angular, Angular Material, Wad, Sass, Gulp, Foundation, AJAX, Dragula',
         },
         {
-            link: 'https://github.com/leowolf749/movie-emporeum',
+            link: 'http://addicted-worm.surge.sh/',
             name: 'Movie Emporeum',
             image: 'assets/movie-emp-scrnshot.png',
             description: 'A movie catalog that pulls recent movies from an API and allows users to rate them. Users can also add movies to the catalog.',
             tools: 'Angular, Sass, Gulp, Foundation, AJAX'
         },
         {
-            link: 'https://github.com/leowolf749/back-to-chat',
+            link: 'http://lewd-wren.surge.sh/',
             name: 'Chatterbox2',
             image: 'assets/chatterbox-scrnshot.png',
             description: 'A minimalistic messaging app that allows users to send and recieve messages through an API.',
             tools: 'JQuery, Backbone, Sass, Gulp, AJAX',
         },
         {
-            link: 'https://github.com/leowolf749/shoppers_delight',
+            link: 'http://icky-advice.surge.sh/',
             name: 'The Happy Mask Shop Online',
             image: 'assets/shoppers-scrnshot.png',
             description: 'A mock website of the Happy Mask Shop from the Zelda video game franchise. Users can browse various masks for sale and add them to their cart. The cart adds up their total.',
